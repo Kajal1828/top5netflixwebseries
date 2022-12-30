fe73b55c72eabd7294fbe14de2fc5d01057ea0e8
kajal <kajalneha.nk@gmail.com>
2022-12-30 22:57:50 +0530

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,34 @@
+const Sdata = [
+    {
+        sname: "DARK",
+        imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title:"Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname: "extra curricular",
+        imgsrc: "https://tse1.explicit.bing.net/th?id=OIP.mpSeChLRvK0y4wu7p7wS2QHaFb&pid=Api&P=0",
+        title:"Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname: "Stranger Thingss",
+        imgsrc: "https://wallpapercave.com/wp/wp1917154.jpg",
+        title:"Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+
+    },
+    {
+        sname: "the Vampire Diaries",
+        imgsrc: "https://images.justwatch.com/poster/8589251/s592",
+        title: "azon original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname: "My First-2 Love",
+        imgsrc: "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+        title:"Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+];
+export default Sdata;
